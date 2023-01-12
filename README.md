baa1acc5b87aa439741a524c8f51bf3e7b4384cc
vijayrajpanchall <vijayrajpanchal4@gmail.com>
2023-01-12 17:13:02 +0530

    Uniswap contracts added

--- README.md
+++ README.md
@@ -1,13 +1,4 @@
-# Sample Hardhat Project
+# DEx(UniswapV2) Task
 
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
-
-Try running some of the following tasks:
-
-```shell
-npx hardhat help
-npx hardhat test
-REPORT_GAS=true npx hardhat test
-npx hardhat node
-npx hardhat run scripts/deploy.js
-```
+## About
+On this decentralized exchange, a fee of 4% is taken from each trade, with 2% coming from the incoming token and 2% coming from the outgoing token. For example, if a user swaps $100 worth of BUSD for USDC, they will receive 96 dollars worth of USDC, and the exchange's reward wallet will receive $2 worth of BUSD and $2 worth of USDC.
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,58 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+        blockNumber: 12000000,
+      },
+    },
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [`${process.env.PRIVATE_KEY}`],
+    },
+    bnbTestnet: {
+      url: `https://data-seed-prebsc-1-s1.binance.org:8545`,
+      accounts: [`${process.env.PRIVATE_KEY}`],
+    },
+  },
+
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+
+  solidity: {
+    compilers: [
+      {
+        version: "0.6.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: "0.5.16",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: "0.4.18",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
 };
